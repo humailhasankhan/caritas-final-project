f44db14436db1cded04f5af4bed568914bd679a3
mohammed <mohammedalabdulhamid@gmail.com>
2022-01-26 13:48:00 +0000

    restyling text

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const AboutUs = () => (
-  <div className="m-2 bg-red-600 font-lato w-full p-4">
-    <div className="bg-red">
-      <h2 className="text-center text-white text-4xl mt-6 w-full bg-red">
+  <div className="m-2 bg-red-600 font-lato w-full p-4 text-white">
+    <div className="bg-red ">
+      <h2 className="text-center  text-4xl mt-6 w-full bg-red">
         Caritas Refugee Education
       </h2>
       <p className="text-gray-600 p-4 bg-white m-6 font-bold">
@@ -11,7 +11,7 @@ const AboutUs = () => (
         getting a qualification with Caritas and now succeeding in my university
         course.” Musaab – former student
       </p>
-      <p className="text-white p-4">
+      <p className=" p-4">
         Caritas Refugee Education works alongside refugees and asylum seekers in
         order to assist in their journey of wellbeing and integration. We do
         this by providing high quality language lessons, delivered in a
